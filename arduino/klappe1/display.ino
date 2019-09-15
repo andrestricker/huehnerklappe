ee13e24fa06e5d23761c77f46663295970ad732e
andrestricker <info@scw-services.com>
2019-09-15 22:15:44 +0200

    Minor changes

--- arduino/klappe1/display.ino
+++ arduino/klappe1/display.ino
@@ -1,4 +1,4 @@
-// set the display. Comparing lines to display with lines already displayed to avoid flickering
+// set the display. Comparing lines to display with lines already displayed to avoid flickering.
 
 void set_display(String line1, String line2){
   if(line1!=buf_line1 or line2!=buf_line2){
